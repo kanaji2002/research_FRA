debfea6990700573ffebf27f520ae0eff2aaee1e
kanaji2002 <gellunika6478@icloud.com>
2024-11-29 23:57:25 +0100

    add picture

--- data/2process/power2.py
+++ data/2process/power2.py
@@ -12,7 +12,7 @@ def categorize_chl_a(values):
     return np.where(values < 30, 0, np.where(values < 40, 1, 2))
 
 # Load data
-data = pd.read_csv('power.csv')
+data = pd.read_csv('research_FRA/data/2process/power_add_param_and_layeres.csv')
 
 # Initialize dictionaries to save MSEs for each model across iterations
 individual_model_results_all = {}
@@ -24,10 +24,11 @@ for _ in range(3):
     train_data, test_data = train_test_split(data, test_size=0.1, random_state=42)
 
     # # 最適そうな変数
-    explanatory_vars = ['MUsage1', 'MUsage2', 'model1_P','model2_P','GPU_Uti', 'M_Uti', 'model1_FLP', 'model2_FLP']
+    explanatory_vars = ['layers_1', 'layers_2', 'batch_size_1','batch_size_2','model1_Param', 'model2_Param', 'model1_FLP', 'model2_FLP']
     
-    
-    # # 先生から言われた変数
+    ### model1,model2,MUsage1,MUsage2,Power,Tem,GPU_Uti,M_Uti,CommonP,model1_P,model2_P,model1+2_P,model1_FLP,model2_FLP,model1_P_2pir,model2_P_2pair,model1_Param,
+    # model2_Param,embedVectorLen_1,embedVectorLen_2,head_1,head_2,dim_feedforward_1,dim_feedforward_2,layers_1,layers_2,batch_size_1,batch_size_2,,,model_py,P_Usage,model1_FLOPS,Parameter,embedVectorLen,head,dim_feedforward,layers,batch_size
+
     # explanatory_vars = ['MUsage1', 'MUsage2', 'model1_FLP', 'model2_FLP']
 
     
@@ -61,21 +62,22 @@ for _ in range(3):
 
 
     # Compare models and get the top 15
-    models_comparison = compare_models(sort='MSE', n_select=18)
+    models_comparison = compare_models(sort='MAE', n_select=18)
     
     # Collect MSE for individual models
     for model in models_comparison:
         predictions = predict_model(model, data=test_data)
-        mse = ((predictions['Power'] - predictions['prediction_label']) ** 2).mean()
+        mae = abs(predictions['Power'] - predictions['prediction_label']).mean()
+
         model_name = model.__class__.__name__
         if model_name not in individual_model_results_all:
             individual_model_results_all[model_name] = []
-        individual_model_results_all[model_name].append(mse)
+        individual_model_results_all[model_name].append(mae)
 
     # Select models for stacking
-    models_list1 = compare_models(sort='MSE', n_select=3)
-    models_list2 = compare_models(sort='MSE', n_select=5)
-    models_list3 = compare_models(sort='MSE', n_select=10)
+    models_list1 = compare_models(sort='MAE', n_select=3)
+    models_list2 = compare_models(sort='MAE', n_select=5)
+    models_list3 = compare_models(sort='MAE', n_select=10)
     
     
     
@@ -83,11 +85,12 @@ for _ in range(3):
     for i, models_list in enumerate([models_list1, models_list2, models_list3], 1):
         stacked_model = stack_models(estimator_list=models_list, meta_model=LinearRegression())
         predictions = predict_model(stacked_model, data=test_data)
-        mse = ((predictions['Power'] - predictions['prediction_label']) ** 2).mean()
+        mae = abs(predictions['Power'] - predictions['prediction_label']).mean()
+
         model_name = f'Stacked_Model_{i}'
         if model_name not in stacked_model_results_all:
             stacked_model_results_all[model_name] = []
-        stacked_model_results_all[model_name].append(mse)
+        stacked_model_results_all[model_name].append(mae)
 
         # Confusion matrix for the first stacked model
         if i == 1:
@@ -105,45 +108,34 @@ for _ in range(3):
         # Collect MSE for individual models
     for model in models_comparison:
         predictions = predict_model(model, data=test_data)
-        
-        # 予測値と実際の値を表示
-        print("Model:", model.__class__.__name__)
-        print(predictions[['Power', 'prediction_label']].head())  # 実際の値と予測値の比較
-
-        mse = ((predictions['Power'] - predictions['prediction_label']) ** 2).mean()
+        mae = abs(predictions['Power'] - predictions['prediction_label']).mean()
         model_name = model.__class__.__name__
         if model_name not in individual_model_results_all:
             individual_model_results_all[model_name] = []
-        individual_model_results_all[model_name].append(mse)
+        individual_model_results_all[model_name].append(mae)
 
     # Stacked model predictions
     for i, models_list in enumerate([models_list1, models_list2, models_list3], 1):
         stacked_model = stack_models(estimator_list=models_list, meta_model=LinearRegression())
         predictions = predict_model(stacked_model, data=test_data)
-        
-        # 予測値と実際の値を表示
-        print(f"Stacked Model {i}")
-        print(predictions[['Power', 'prediction_label']].head())  # 実際の値と予測値の比較
-
-        mse = ((predictions['Power'] - predictions['prediction_label']) ** 2).mean()
+        mae = abs(predictions['Power'] - predictions['prediction_label']).mean()
         model_name = f'Stacked_Model_{i}'
         if model_name not in stacked_model_results_all:
             stacked_model_results_all[model_name] = []
-        stacked_model_results_all[model_name].append(mse)
+        stacked_model_results_all[model_name].append(mae)
 
-
-# Calculate average MSE for each model
-average_mse_results = {
-    model: np.mean(mse_list) for model, mse_list in {**individual_model_results_all, **stacked_model_results_all}.items()
+# Calculate average MAE for each model
+average_mae_results = {
+    model: np.mean(mae_list) for model, mae_list in {**individual_model_results_all, **stacked_model_results_all}.items()
 }
 
 # Convert the results into a DataFrame for plotting
-average_mse_df = pd.DataFrame(list(average_mse_results.items()), columns=['Model', 'Average MSE'])
+average_mae_df = pd.DataFrame(list(average_mae_results.items()), columns=['Model', 'Average MAE'])
 
-# Plot MSE comparison
+# Plot MAE comparison
 plt.figure(figsize=(12, 6))
-bars = plt.barh(average_mse_df['Model'], average_mse_df['Average MSE'], color='skyblue')
-plt.xlabel('Average MSE')
+bars = plt.barh(average_mae_df['Model'], average_mae_df['Average MAE'], color='skyblue')
+plt.xlabel('Average MAE')
 plt.ylabel('Model')
 # plt.title('Average Model Comparison on Test Data (MSE) over 10 iterations')
 plt.gca().invert_yaxis()
@@ -153,7 +145,7 @@ plt.subplots_adjust(left=0.15)  # 左側に余白を追加
 
 
 # 右側に余白を追加
-plt.xlim(0, max(average_mse_df['Average MSE']) * 1.1)
+plt.xlim(0, max(average_mae_df['Average MAE']) * 1.1)
 
 # 各バーにMSEの値を表示
 for bar in bars:
